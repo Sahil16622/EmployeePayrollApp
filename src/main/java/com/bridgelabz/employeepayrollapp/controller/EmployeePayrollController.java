34d308b8fa82bc3258d395459c2fa074d97f61f6
Sahil Singh Chauhan <sahilchauhan5467@gmail.com>
2025-02-27 13:55:48 +0530

    Sahil, UC-04: Introduced Service Layer for Employee Payroll Project.

--- src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeePayrollController.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeePayrollController.java
@@ -1,35 +1,34 @@
 package com.bridgelabz.employeepayrollapp.controller;
 
-
-
 import com.bridgelabz.employeepayrollapp.dto.EmployeeDTO;
 import com.bridgelabz.employeepayrollapp.model.Employee;
+import com.bridgelabz.employeepayrollapp.service.IEmployeePayrollService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/employeepayrollservice")
 public class EmployeePayrollController {
-    private List<Employee> employeeList = new ArrayList<>();
-    private int counter = 1;
+    @Autowired
+    private IEmployeePayrollService employeePayrollService;
 
     // Get All Employees
     @GetMapping("/")
     public ResponseEntity<List<Employee>> getAllEmployees() {
+        List<Employee> employeeList = employeePayrollService.getAllEmployees();
         return new ResponseEntity<>(employeeList, HttpStatus.OK);
     }
 
     // Get Employee by ID
     @GetMapping("/get/{id}")
     public ResponseEntity<Employee> getEmployeeById(@PathVariable int id) {
-        for (Employee employee : employeeList) {
-            if (employee.getId() == id) {
-                return new ResponseEntity<>(employee, HttpStatus.OK);
-            }
+        Employee employee = employeePayrollService.getEmployeeById(id);
+        if (employee != null) {
+            return new ResponseEntity<>(employee, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
@@ -37,20 +36,16 @@ public class EmployeePayrollController {
     // Create New Employee
     @PostMapping("/create")
     public ResponseEntity<Employee> createEmployee(@RequestBody EmployeeDTO employeeDTO) {
-        Employee newEmployee = new Employee(counter++, employeeDTO);
-        employeeList.add(newEmployee);
+        Employee newEmployee = employeePayrollService.createEmployee(employeeDTO);
         return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
     }
 
     // Update Employee by ID
     @PutMapping("/update/{id}")
-    public ResponseEntity<Employee> updateEmployee(@PathVariable int id, @RequestBody Employee updatedEmployee) {
-        for (Employee employee : employeeList) {
-            if (employee.getId() == id) {
-                employee.setName(updatedEmployee.getName());
-                employee.setSalary(updatedEmployee.getSalary());
-                return new ResponseEntity<>(employee, HttpStatus.OK);
-            }
+    public ResponseEntity<Employee> updateEmployee(@PathVariable int id, @RequestBody EmployeeDTO employeeDTO) {
+        Employee updatedEmployee = employeePayrollService.updateEmployee(id, employeeDTO);
+        if (updatedEmployee != null) {
+            return new ResponseEntity<>(updatedEmployee, HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
@@ -58,12 +53,7 @@ public class EmployeePayrollController {
     // Delete Employee by ID
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<String> deleteEmployee(@PathVariable int id) {
-        for (Employee employee : employeeList) {
-            if (employee.getId() == id) {
-                employeeList.remove(employee);
-                return new ResponseEntity<>("Employee Deleted", HttpStatus.OK);
-            }
-        }
-        return new ResponseEntity<>("Employee Not Found", HttpStatus.NOT_FOUND);
+        employeePayrollService.deleteEmployee(id);
+        return new ResponseEntity<>("Employee Deleted", HttpStatus.OK);
     }
 }
